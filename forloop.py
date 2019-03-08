c2fb7b2a4ef39a15a730bebe59db3c171c86f7fb
Noordeen Malango <noordinmalango@gmail.com>
2019-03-08 22:23:15 +0200

    for loop code

---
+++ forloop.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  8 22:06:59 2019
+
+@author: nmalango
+"""
+
+#implementing the for loop using range function
+for i in range(5):
+    print(i)
+
+#changing the starting point inn the range function
+for i in range(1,5):
+    print(i)
+
+#changing the increment value in the loop using the range function
+for i in range(1,30,2):
+    print(i)
+#printing the values in a descending order
+for i in range(30,1,-1):
+    print(i)
