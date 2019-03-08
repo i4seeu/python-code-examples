71c959357424c642add5a8c7f36fbaccd5c772f0
Noordeen Malango <noordinmalango@gmail.com>
2019-03-08 21:54:14 +0200

    introduction to pytho 3

---
+++ basics.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  8 21:39:44 2019
+
+@author: nmalango
+"""
+#printing out something to the screen
+
+print("Welcome to the basics of python programming language")
+
+#how to get an input from the user
+
+name = input("Enter your name here: ")
+print("Your name is :", name)
+
+#To get a number from the user input we use the eval fuction
+
+number = eval(input("Enter a number :"))
+print("The square of the numbeer you have entered is : ",number*number)
+
+
